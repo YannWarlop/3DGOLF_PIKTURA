fb03e0f1c891771d2017caf61dec2c9b8a74b84f
Yann-Warlop <yann.warlop@outlook.fr>
2025-03-08 14:55:41 +0100

    08/03 All Levels

--- Assets/Scripts/GPE/LightToggler.cs
+++ Assets/Scripts/GPE/LightToggler.cs
@@ -2,15 +2,23 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LightToggler : MonoBehaviour
 {
     [Header("References")] 
     [SerializeField] private List<GameObject> _lights;
     [SerializeField] private Animator _animator;
+    [SerializeField] private WindZone_PushPull _windZone;
     
     [Header("References - Sounds")]
     [SerializeField] private AudioClip _toggleSound;
+
+    private void Start()
+    {
+        //BRUTEFORCE SALE
+        if (SceneManager.GetActiveScene().name == "LV5") _animator.SetTrigger("ToggledLV5");
+    }
 
     private void OnCollisionEnter(Collision other) {
         if (other.gameObject.CompareTag("Player")) {
@@ -19,7 +27,12 @@ public class LightToggler : MonoBehaviour
             {
                 if (_light != null) _light.SetActive(true);
             }
-            if (_animator != null) _animator.SetTrigger("Toggled");
+
+            if (_animator != null) {
+                _animator.SetTrigger($"Toggled{SceneManager.GetActiveScene().name}");
+                
+            }
+            if (_windZone != null) _windZone._speedMult = 2.5f;
         }
     }
 }
--- Assets/Scripts/GPE/WindZone_PushPull.cs
+++ Assets/Scripts/GPE/WindZone_PushPull.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 
 public class WindZone_PushPull : MonoBehaviour
 {
+    [SerializeField] public float _speedMult = 1f;
+
+    private void Update()
+    {
+        transform.RotateAround(transform.position, Vector3.up, 50f * (_speedMult * 10) * Time.deltaTime); 
+    }
+    
     private void OnTriggerStay(Collider _other) {
         Debug.Log("Trigger");
         Debug.Log(_other.gameObject.name);
         if (_other.gameObject.CompareTag("Player")) {
-            _other.gameObject.GetComponent<Rigidbody>().AddForce(transform.up * 15);
+            _other.gameObject.GetComponent<Rigidbody>().AddForce((transform.up * 15) * _speedMult);
             Debug.Log("Force");
         }
     }
